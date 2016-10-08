c0a3195c2bda2187888b8ce3113a300055a3947f
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-10-08 13:40:56 -0400

    Now I understand how meteor works

--- questiveMeteor/imports/ui/App.jsx
+++ questiveMeteor/imports/ui/App.jsx
@@ -44,9 +44,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+      return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+ 
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
@@ -93,6 +102,9 @@ App.propTypes = {
 };
 
 export default createContainer(() => {
+  //Subscribe to tasks 
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
