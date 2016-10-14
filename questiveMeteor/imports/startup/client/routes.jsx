17630ed665e72e9e41aaf626d45c38c89c7d9bc1
Todd Germaine <trgermaine@gmail.com>
2016-10-13 20:36:55 -0400

    fixed routing bug

--- questiveMeteor/imports/startup/client/routes.jsx
+++ questiveMeteor/imports/startup/client/routes.jsx
@@ -4,6 +4,10 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { App } from '../../ui/layouts/app.jsx';
 import { Index } from '../../ui/index.jsx';
 
+import { Create } from '../../ui/Children/create_hunt.jsx';
+import { Leaderboard } from '../../ui/Children/Leaderboard.jsx';
+import { Task } from '../../ui/Children/Task.jsx';
+
 // import { One } from '../../ui/pages/one.jsx';
 // import { Two } from '../../ui/pages/two.jsx';
 
@@ -13,9 +17,18 @@ const routes = (
     <Router history={ browserHistory }>
       <Route path="/" component={ App }>
        <IndexRoute component={ Index } />
+
+        <Route path="/create" component={ Create } />
+        <Route path="/leaderboard" component={ Leaderboard } />
+        <Route path="/task" component={ Task } />
+
+
       </Route>
    
     </Router>
+
+
+
     );
 
 export default routes;
