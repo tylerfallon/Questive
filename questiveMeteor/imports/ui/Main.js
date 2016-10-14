0eedc16c5e523aab5c9a8ed1a1cb7e40e859aaaf
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro-2.local>
2016-10-13 20:24:47 -0400

    components ftw

---
+++ questiveMeteor/imports/ui/Main.js
@@ -0,0 +1,95 @@
+// Include React and React-Router dependencies
+var React = require('react');
+var Router = require('react-router')
+
+
+var bgColors = { "Default": "#e3f2fd",
+                    "Blue": "#00B1E1",
+                    "Cyan": "#37BC9B",
+                    "Green": "#8CC152",
+                    "Red": "#E9573F",
+                    "Yellow": "#F6BB42",
+};
+
+var Main = React.createClass({
+
+	render: function(){
+
+		return(
+			/*We can only render a single div. So we need to group everything inside of this main-container one*/
+			<div className="main-container">
+
+
+				<div className="container">
+					{/*Navbar*/}
+					<nav className="navbar navbar-dark bg-info">
+						<div className="container-fluid">
+							<div className="navbar-header">
+								<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+									<span className="sr-only">Toggle navigation</span>
+									<span className="icon-bar"></span>
+									<span className="icon-bar"></span>
+									<span className="icon-bar"></span>
+								</button>
+							</div>
+					
+							<div className="collapse navbar-collapse navbar-ex1-collapse">
+											
+									<ul className="nav navbar-nav navbar-left">
+									<li><img src={'https://s-media-cache-ak0.pinimg.com/originals/e2/5c/43/e25c43c6a65bdca84c72f0c58524fcd6.png'} height='50' width='50'/></li>
+									<li><a href="#/index">Home</a></li>
+									</ul>
+					
+
+
+
+						
+									
+
+							
+
+									<ul className="nav navbar-nav navbar-right">
+									<li><img src={'http://blog.roblox.com/wp-content/uploads/2014/04/Points-Icon-700px.png'} height='50' width='50'/></li>
+									<li><a href="#/Create">Create</a></li>
+									</ul>
+
+	<ul className="nav navbar-nav navbar-right">
+									<li><img src={'http://www.engage2excel.com/wp-content/uploads/2015/09/Contests-icon.png'} height='50' width='50'/></li>
+									<li><a href="#/join">Join</a></li>
+									</ul>
+
+							<ul className="nav navbar-nav navbar-right">
+									<li><img src={'http://www.freeiconspng.com/uploads/leaderboard-icon-3.png'} height='50' width='50'/></li>
+									<li><a href="#/leaderboards">Leaderboards</a></li>
+								</ul>
+
+			<ul className="nav navbar-nav navbar-right navbar-for-responsiveness">
+									<li><img src={'https://openclipart.org/download/247319/abstract-user-flat-3.svg'} height='50' width='50'/></li>
+									<li><a href="#/profile">Profile</a></li>
+									</ul>
+							</div>
+						</div>
+					</nav>
+
+
+
+
+
+
+
+					{/*Here we will load the sub components (Search or Saved */}
+					{/*These sub-components are getting passed as this.props.children*/}
+					{this.props.children}
+
+		
+				</div>
+
+
+
+			</div>
+		)
+	}
+});
+
+// Export the module back to the route
+module.exports = Main;
---
+++ questiveMeteor/imports/ui/Search/Query.js
@@ -0,0 +1,114 @@
+// Include React and React-Router dependencies
+var React = require('react');
+import { PageHeader } from 'react-bootstrap';
+
+// Query Component Declaration
+var Query = React.createClass({ 
+
+	// Here we set initial variables for the component to be blanks
+	getInitialState: function(){
+		return {
+			search: ""
+		}
+	},
+
+	// Whenever we detect ANY change in the textbox, we register it. 
+    handleChange: function(event) {
+    	console.log("TEXT CHANGED");
+
+    	// Here we create syntax to capture any change in text to the query terms (pre-search).
+    	// See this Stack Overflow answer for more details: 
+    	// http://stackoverflow.com/questions/21029999/react-js-identifying-different-inputs-with-one-onchange-handler
+    	var newState = {};
+    	newState[event.target.id] = event.target.value;
+    	this.setState(newState);
+    },
+
+	/*This code handles the sending of the search terms to the parent Search component*/
+	handleSubmit: function(){
+		console.log("CLICKED");
+		this.props.updateSearch(this.state.search);
+		return false;
+	},
+
+	// Here we render the Query component
+	render: function(){
+
+		return(
+			<div className ="main-container">
+
+					<div className="row">
+						<div className="col-lg-12">
+
+							<div className="navbar navbar-light bg-default">
+								<div className="panel-heading">
+									<h1 className="panel-title"><strong><i className="fa fa-newspaper-o" aria-hidden="true"></i>  Search for a Contest</strong></h1>
+								</div>
+								<div className="panel-body">
+
+									{/*Note how we associate the text-box inputs with the state values*/}
+									<form>
+										<div className="form-group">
+											<h4 className=""><strong>Topic</strong></h4>
+											<input type="text" value={this.state.value} className="form-control " id="search" onChange= {this.handleChange} required/>
+
+								
+										</div>
+
+										
+										<div className="pull-left">
+											<button type="button" className="btn btn-danger" onClick={this.handleSubmit}><h4>Submit</h4></button>
+										</div>
+									</form>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+<center><PageHeader>OR</PageHeader></center>
+					<div className="row">
+						<div className="col-lg-12">
+
+							<div className="navbar navbar-light bg-default">
+								<div className="panel-heading">
+									<h1 className="panel-title"><strong><i className="fa fa-newspaper-o" aria-hidden="true"></i>  See All Current Contests</strong></h1>
+								</div>
+								<div className="panel-body">
+
+									{/*Note how we associate the text-box inputs with the state values*/}
+									<form>
+										<div className="form-group">
+	
+										</div>
+
+										
+										<div className="pull-left">
+											<button type="button" className="btn btn-danger" onClick={this.handleSubmit}><h4>Populate List</h4></button>
+										</div>
+									</form>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+
+			</div>
+
+
+
+
+
+
+
+
+
+		)
+	}
+
+
+});
+
+// Export the module back to the route
+module.exports = Query;
