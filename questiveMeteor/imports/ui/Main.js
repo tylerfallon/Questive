e648eed73cbc199932bdb04881fec2ad638dbf71
Todd Germaine <trgermaine@gmail.com>
2016-10-14 02:16:15 -0400

    added logo area and fixed navbar

--- questiveMeteor/imports/ui/Main.js
+++ questiveMeteor/imports/ui/Main.js
@@ -2,7 +2,6 @@
 var React = require('react');
 var Router = require('react-router')
 
-
 var bgColors = { "Default": "#e3f2fd",
                     "Blue": "#00B1E1",
                     "Cyan": "#37BC9B",
@@ -17,13 +16,12 @@ var Main = React.createClass({
 
 		return(
 			/*We can only render a single div. So we need to group everything inside of this main-container one*/
-			<div className="main-container">
-
-
+		<div className="row">
 				<div className="container">
 					{/*Navbar*/}
 					<nav className="navbar navbar-dark bg-info">
 						<div className="container-fluid">
+
 							<div className="navbar-header">
 								<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 									<span className="sr-only">Toggle navigation</span>
@@ -31,41 +29,50 @@ var Main = React.createClass({
 									<span className="icon-bar"></span>
 									<span className="icon-bar"></span>
 								</button>
+								<img src={'https://s10.postimg.org/dfh868909/Questive_Logo.png'} height='50' width='150' className="logoArea"/>
 							</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 					
 							<div className="collapse navbar-collapse navbar-ex1-collapse">
 											
-									<ul className="nav navbar-nav navbar-left">
-									<li><img src={'https://s-media-cache-ak0.pinimg.com/originals/e2/5c/43/e25c43c6a65bdca84c72f0c58524fcd6.png'} height='50' width='50'/></li>
-									<li><a href="#/index">Home</a></li>
-									</ul>
-					
-
-
-
-						
 									
 
-							
 
 									<ul className="nav navbar-nav navbar-right">
-									<li><img src={'http://blog.roblox.com/wp-content/uploads/2014/04/Points-Icon-700px.png'} height='50' width='50'/></li>
+									<li><img src={'http://blog.roblox.com/wp-content/uploads/2014/04/Points-Icon-700px.png'} height='48' width='48'/></li>
 									<li><a href="/Create">Create</a></li>
 									</ul>
 
 	<ul className="nav navbar-nav navbar-right">
-									<li><img src={'http://www.engage2excel.com/wp-content/uploads/2015/09/Contests-icon.png'} height='50' width='50'/></li>
+									<li><img src={'http://www.engage2excel.com/wp-content/uploads/2015/09/Contests-icon.png'} height='48' width='48'/></li>
 									<li><a href="/join">Join</a></li>
 									</ul>
 
 							<ul className="nav navbar-nav navbar-right">
-									<li><img src={'http://www.freeiconspng.com/uploads/leaderboard-icon-3.png'} height='50' width='50'/></li>
+									<li><img src={'http://www.freeiconspng.com/uploads/leaderboard-icon-3.png'} height='48' width='48'/></li>
 									<li><a href="/Leaderboards">Leaderboards</a></li>
 								</ul>
 
 			<ul className="nav navbar-nav navbar-right navbar-for-responsiveness">
-									<li><img src={'https://openclipart.org/download/247319/abstract-user-flat-3.svg'} height='50' width='50'/></li>
+									<li><img src={'https://openclipart.org/download/247319/abstract-user-flat-3.svg'} height='48' width='48'/></li>
 									<li><a href="/profile">Profile</a></li>
+									</ul>
+
+									<ul className="nav navbar-nav navbar-right">
+										<li><img src={'https://s-media-cache-ak0.pinimg.com/originals/e2/5c/43/e25c43c6a65bdca84c72f0c58524fcd6.png'} height='48' width='48'/></li>
+										<li><a href="#/index">Home</a></li>
 									</ul>
 							</div>
 						</div>
@@ -75,10 +82,7 @@ var Main = React.createClass({
 
 		
 				</div>
-
-
-
-			</div>
+		</div>
 		)
 	}
 });
