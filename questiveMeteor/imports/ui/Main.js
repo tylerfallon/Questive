367233d59c49c103ccb61ca04817c8f784e2ca30
Todd Germaine <trgermaine@gmail.com>
2016-10-15 01:47:21 -0400

    fixed bg

--- questiveMeteor/imports/ui/Main.js
+++ questiveMeteor/imports/ui/Main.js
@@ -31,25 +31,9 @@ var Main = React.createClass({
 								</button>
 								<a href='/profile'><img src={'https://s10.postimg.org/dfh868909/Questive_Logo.png'} height='50' width='150' className="logoArea"/></a>
 							</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
 					
 							<div className="collapse navbar-collapse navbar-ex1-collapse">
 											
-									
-
-
 									<ul className="nav navbar-nav navbar-right">
 									<li><img src={'http://blog.roblox.com/wp-content/uploads/2014/04/Points-Icon-700px.png'} height='48' width='48'/></li>
 									<li><a href="/Create">Create</a></li>
@@ -73,9 +57,66 @@ var Main = React.createClass({
 						</div>
 					</nav>
 
+
+
+
 					{this.props.children}
 
-		
+				
+						<div className="main-container"> 
+        <div className="parallax">
+          <div id="group2" className="parallax__group">
+            <div className="parallax__layer parallax__layer--base">
+              <center><h3 className="tagline">Discover. Connect. Adventure. <br /> <br />
+              <center><button type="button" className="btn btn-danger center" onClick={this.handleSubmit}><h4>Get Started</h4></button></center><br /></h3></center>                
+
+               <br /><br /> <br />
+             <center><h1 className='center'>Welcome to Questive.</h1></center>
+            </div>
+            <a name="about" />
+            <div className="parallax__layer parallax__layer--back black">
+            </div>
+          </div>
+          
+          <div id="group5" className="parallax__group">
+            <div className="parallax__layer parallax__layer--fore">
+              <div className="title"></div>
+            </div>
+            <div className="parallax__layer parallax__layer--base">
+              <div className="title">  <div className="card-panel" id="about">
+                    <h3 className="center"><p className='left whatIs'>What is Questive?</p></h3>
+                    <p className="center whatIs">Questive is a social web and mobile application that allows you to explore the world around you.</p>
+                    <p className="center whatIs">
+                      Join contests created by other users or create one yourself!
+                    </p>
+                  </div>
+              </div>
+            </div>
+          </div>
+			
+
+          <div id="group7" className="parallax__group">
+            <div className="parallax__layer parallax__layer--fore">
+              <div className="title"></div>
+            </div>
+            <div className="parallax__layer parallax__layer--base">
+              <div className="title">  <div className="card-panel col s12" id="about">
+                    <h3 className="center"><p className='left'>Learn more about us.</p></h3>
+                    <p className="center">Please learn more.</p>
+                    <p className="center">
+                      Pls.
+                    </p>
+                  </div>
+              </div>
+            </div>
+          </div>
+
+			</div>
+
+
+
+			</div>
+
 				</div>
 		</div>
 		)
