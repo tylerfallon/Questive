26dcb01697ec859ae5a40f9d5d4a042f6507edd3
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-10-15 10:44:00 -0400

    working on bootstrap'

--- questiveMeteor/client/main.jsx
+++ questiveMeteor/client/main.jsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 //start startup files
 import '../imports/startup/accounts-config.js';
 
+import App from '../imports/ui/App.jsx';
 import Main from '../imports/ui/Main.js';
 import routes from '../imports/startup/client/routes';
 
 
 Meteor.startup(() => {
-	ReactDOM.render(routes ,document.getElementById("app"));
+	ReactDOM.render(Main, document.getElementById("app"));
 });
